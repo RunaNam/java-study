2c160ee8f8ac3f3cd50420112308355289e4694e
RunaNam <nmj5012@naver.com>
2022-03-25 12:13:17 +0900

    0325 수업 과제

--- src/main/java/techcourse/fp/mission/StreamStudy.java
+++ src/main/java/techcourse/fp/mission/StreamStudy.java
@@ -7,51 +7,51 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class StreamStudy {
 
     public static long countWords() throws IOException {
         String contents = Files.readString(Paths
             .get("src/main/resources/fp/war-and-peace.txt"));
-        List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        long count = 0;
-        for (String w : words) {
-            if (w.length() > 12) count++;
-        }
-        return count;
+        return Arrays.stream(contents.split("[\\P{L}]+"))
+            .filter(word -> word.length() > 12)
+            .count();
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
-        List<Integer> result = new ArrayList<>();
-        for (Integer number : numbers) {
-            result.add(2 * number);
-        }
-
-        return result;
+        return numbers.stream()
+            .map(integer -> integer * 2)
+            .collect(Collectors.toList());
     }
 
     public static long sumAll(List<Integer> numbers) {
-        int result = 0;
-
-        for (Integer number : numbers) {
-            result += number;
-        }
-
-        return result;
+        return numbers.stream()
+            .mapToInt(num -> num)
+            .sum();
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0L; // TODO: 이 부분을 구현한다.
+        return numbers.stream()
+            .mapToLong(num -> num)
+            .filter(num -> num > 3)
+            .map(num -> num * 2)
+            .sum();
     }
 
     public static void printLongestWordTop100() throws IOException {
         String contents = Files.readString(Paths
             .get("src/main/resources/fp/war-and-peace.txt"));
-        List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
+
+        List<String> words = Arrays.stream(contents.split("[\\P{L}]+"))
+            .filter(word -> word.length() > 12)
+            .distinct()
+            .limit(100)
+            .collect(Collectors.toList());
 
         System.out.println(words);
         System.out.println(words.size());
-        // TODO 이 부분에 구현한다.
     }
 }
